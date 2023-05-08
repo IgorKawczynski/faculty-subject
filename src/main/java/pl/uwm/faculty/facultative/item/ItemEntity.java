b5fcf765cd193540359784ee81c45d2be976a77c
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-05-08 21:53:24 +0200

    Added user's items entity, modified user's repository access modifier

---
+++ src/main/java/pl/uwm/faculty/facultative/item/ItemEntity.java
@@ -0,0 +1,48 @@
+package pl.uwm.faculty.facultative.item;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.LazyToOne;
+import org.hibernate.annotations.LazyToOneOption;
+import pl.uwm.faculty.facultative.global.GlobalEntity;
+import pl.uwm.faculty.facultative.user.UserEntity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ *  Entity of user's items
+ */
+@Table(name = "items")
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ItemEntity extends GlobalEntity implements Serializable {
+
+    String itemName;
+    String description;
+    Double price;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @LazyToOne(LazyToOneOption.NO_PROXY)
+    @JoinColumn(name = "user_id", nullable = true)
+    @JsonBackReference
+    UserEntity userId;
+
+    @Builder
+    public ItemEntity(
+            String itemName,
+            String description,
+            Double price,
+            UserEntity userId
+
+    ) {
+        this.itemName = itemName;
+        this.description = description;
+        this.price = price;
+        this.userId = userId;
+    }
+}
--- src/main/java/pl/uwm/faculty/facultative/user/UserRepository.java
+++ src/main/java/pl/uwm/faculty/facultative/user/UserRepository.java
@@ -2,5 +2,5 @@ package pl.uwm.faculty.facultative.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
 }
