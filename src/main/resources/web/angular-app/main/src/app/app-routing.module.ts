4a1f23f0342d9f9299e70fbcb588e5fa6c638ee7
IgorKawczynski <kluseczkibabuni94@gmail.com>
2023-05-09 15:25:05 +0200

    Added implementation to global app component

--- src/main/resources/web/angular-app/main/src/app/app-routing.module.ts
+++ src/main/resources/web/angular-app/main/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from "./users/users.component";
+import { ItemsComponent } from "./items/items.component";
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: 'users', component: UsersComponent },
+  { path: 'items', component: ItemsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/main/resources/web/angular-app/main/src/app/app.module.ts
+++ src/main/resources/web/angular-app/main/src/app/app.module.ts
@@ -1,17 +1,31 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { ItemsComponent } from './items/items.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ButtonModule } from "primeng/button"
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TableModule } from 'primeng/table';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    ItemsComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        RouterModule,
+        BrowserAnimationsModule,
+      ButtonModule, TableModule
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
